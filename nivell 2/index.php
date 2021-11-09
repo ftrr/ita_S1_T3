44bb49a300ae447f1ab96c9055cc5c9a053688af
ftrr <ftrrbnnn@gmail.com>
2021-11-09 17:23:26 +0100

    Nivell 2 - Exercici 2

--- nivell 2/index.php
+++ nivell 2/index.php
@@ -12,4 +12,21 @@ function suma($s1,$s2){
 
 echo suma(1,2). "<br>";
 echo suma(3,2). "<br>";
-echo suma(2,2). "<br>";
+echo suma(2,2). "<br><br>";
+
+
+
+//Exercici 2
+
+function intercanvi($cadena){
+     $firstLetter = substr($cadena,0,1);
+     $lastLetter = substr($cadena,-1,1);
+     $novaCadena = substr_replace($cadena,$lastLetter,0,1);
+     $novaCadena = substr_replace($novaCadena,$firstLetter,-1,1);
+     return $novaCadena;
+
+}
+
+echo intercanvi("wxyz"). "<br>";
+echo intercanvi("a"). "<br>";
+echo intercanvi("ab"). "<br><br>";
