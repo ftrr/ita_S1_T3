1f63a499faccfcebc140cba704b2b1452ef65746
ftrr <ftrrbnnn@gmail.com>
2021-11-09 13:27:19 +0100

    Exercici 5 + Correccio Exercici 4 (modul %)

--- nivell1/index.php
+++ nivell1/index.php
@@ -37,18 +37,26 @@ echo "Valor: x=" .$x . " " . "Y=" . $y . "<br>";
 echo "Suma: " . ($x + $y) . "<br>"; 
 echo "Resta: " . ($x - $y) . "<br>";
 echo "Producte: " . ($x * $y) . "<br>";
-echo "Mòdul: " . ($x / $y) . "<br><br>"; 
+echo "Mòdul: " . ($x % $y) . "<br><br>"; 
 
 echo "Valor: x=" .$n . " " . "Y=" . $m . "<br>"; 
 echo "Suma: " . ($n + $m) . "<br>"; 
 echo "Resta: " . ($n - $m) . "<br>";
 echo "Producte: " . ($n * $m) . "<br>";
-echo "Mòdul: " . ($n / $m) . "<br><br>"; 
+echo "Mòdul: " . ($n % $m) . "<br><br>"; 
 
 echo "Doble: " . ($x*2) . ", ". ($y*2) . ", ". ($n*2) . ", ". ($m*2). "<br>";
 echo "Suma tots: " .($x+$y+$n+$m). "<br>";
-echo "Producte tots: " .($x*$y*$n*$m). "<br>";
+echo "Producte tots: " .($x*$y*$n*$m). "<br><br>";
 
+
+//Ex5
+$array1 = array(1,2,3,4,5);
+$array2 = array(6,7,8);
+
+array_push($array2, "9");
+array_merge($array1,$array2);
+print_r(array_merge($array1,$array2));
 
 
 
